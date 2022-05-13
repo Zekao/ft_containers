de5df28719dcafc1c8820e6461f7d72d02993f44
Elidjah MAUGALEM <emaugale@e3r5p20.clusters.42paris.fr>
2022-05-14 00:33:30 +0200

    aled

--- red_blak_tree_implement.cpp
+++ red_blak_tree_implement.cpp
@@ -57,6 +57,23 @@ class RedBlackTree
 			_root = _null;
 		}
 		/*==========================================*/
+		/*					Comparaison				*/
+		/*==========================================*/
+		NodePtr min(NodePtr node)
+		{
+			while (node->leftChild != _null)
+				node = node->leftChild;
+			return (node);
+		}
+		
+		NodePtr max(NodePtr node)
+		{
+			while (node->rightChild != _null)
+				node = node->rightChild;
+			return (node);
+		}
+
+		/*==========================================*/
 		/*					Rotations				*/
 		/*==========================================*/
 		void	leftRotate(NodePtr x)
@@ -143,11 +160,163 @@ class RedBlackTree
 				}
 				else
 				{
-					
+					u = k->parent->parent->rightChild;
+					if (u->color == RED)
+					{
+						u->color = BLACK;
+						k->parent->color = BLACK;
+						k->parent->parent->color = RED;
+						k = k->parent->parent;
+					}
+					else
+					{
+						if (k == k->parent->rightChild)
+						{
+							k = k->parent;
+							leftRotate(k);
+						}
+					}
+					k->parent->color = BLACK;
+					k->parent->color = RED;
+					rightRotate(k->parent->parent);
 				}
 			}
 			
 		}
+
+		void	Transplant(NodePtr u, NodePtr v)
+		{
+			if (u->parent == _null)
+				_root = v;
+			else if (u == u->parent->leftChild)
+				u->parent->leftChild = v;
+			else
+				u->parent->rightChild = v;
+			v->parent = u->parent;
+		}
+
+		void	deleteNodeHelper(NodePtr node, int data)
+		{
+			NodePtr z = _null;
+			NodePtr x;
+			NodePtr y;
+			while (node != _null)
+			{
+				if (node->data == data)
+					z = node;
+
+				if (node->data <= data)
+					node = node->rightChild;
+				else
+					node = node->leftChild;
+			}
+
+			if (z == _null)
+			{
+				std::cout << "Key not found" << std::endl;
+				return ;
+			}
+
+			y = z;
+			int tmp = y->color;
+			if (z->leftChild == _null)
+			{
+				x = z->rightChild;
+				Transplant(z, z->rightChild);
+			}
+			else if (z->rightChild == _null)
+			{
+				x = z->leftChild;
+				Transplant(z, z->leftChild);
+			}
+			else
+			{
+				y = min(z->rightChild);
+				tmp = y->color;
+				x = y->rightChild;
+				if (y->parent == z)
+					x->parent = y;
+				else
+				{
+					Transplant(y, y->rightChild);
+					y->rightChild = z->rightChild;
+					y->rightChild->parent = y;
+				}
+
+				Transplant(z, y);
+				y->leftChild = z->leftChild;
+				y->leftChild->parent = y;
+				y->color = z->color;
+			}
+			delete z;
+			if (tmp == BLACK)
+				deleteFix(x);
+		}
+
+		void	deleteNode(int data)
+		{
+			deleteNodeHelper(this->_root, data);
+		}
+
+		void	deleteFix(NodePtr x)
+		{
+			NodePtr	s;
+
+			while (x != _root && x->color == BLACK)
+			{
+				if (x == x->parent->leftChild)
+				{
+					s = x->parent->rightChild;
+					if (s->color == RED)
+					{
+						s->color = BLACK;
+						x->parent->color = RED;
+						leftRotate(x->parent);
+						s = x->parent->rightChild;
+					}
+
+					if (s->leftChild->color == BLACK && s->rightChild->color == 0)
+					{
+						s->color = 1;
+						x = x->parent;
+					}
+					else
+					{
+						if (s->rightChild->color == BLACK)
+						{
+							s->leftChild->color = BLACK;
+							s->color = RED;
+							rightRotate(s);
+							s = x->parent->rightChild;
+						}
+
+						s->color = x->parent->color;
+						x->parent->color = BLACK;
+						s->rightChild->color = BLACK;
+						x = _root;
+					}
+				}
+
+				else
+				{
+					s = x->parent->leftChild;
+					if (s->color == RED)
+					{
+						s->color = BLACK;
+						x->parent->color = RED;
+						rightRotate(x->parent);
+						s = x->parent->leftChild;
+					}
+
+					if (s->rightChild->color == BLACK && s->rightChild->color == BLACK)
+					{
+						s->color = RED;
+						x = x->parent
+					}
+				}
+			}
+		}
+
 		void	insert(int	key)
 		{
 			NodePtr node = new Node;
@@ -162,13 +331,11 @@ class RedBlackTree
 			{
 				y = x;
 				if (node->data < x->data)
-				{
 					x = x->leftChild;
-				}
 				else
 					x = x->rightChild;
 			}
-
+	
 			node->parent = y;
 			if (y == NULL)
 			{
@@ -176,10 +343,7 @@ class RedBlackTree
 				return ;
 			}
 			if (node->parent->parent == NULL)
-			{
 				return ;
-			}
-
 			insertFix(node);
 		}
 	public:
