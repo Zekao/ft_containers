39019a5541806baa29aa4cd86a8d694f2576d74a
Elidjah MAUGALEM <emaugale@e1r12p10.clusters.42paris.fr>
2022-05-25 16:50:12 +0200

    ft::make_pair implemented

--- main.cpp
+++ main.cpp
@@ -4,9 +4,29 @@
 #include "vector.hpp"
 #include <iostream>
 #include "stack.hpp"
+#include "map.hpp"
 #include "iterators/iterator.hpp"
 #include "iterators/iterators_traits.hpp"
 #include "iterators/random_access_iterator.hpp"
+#include "iterators/bidirectionnal_iterator.hpp"
+#include "utils/make_pair.hpp"
+
+int	main(void)
+{
+	ft::pair <int,int> foo;
+	ft::pair <int,int> bar;
+
+	foo = ft::make_pair (10,20);
+	bar = ft::make_pair (10.5,'A'); // ok: implicit conversion from pair<double,char>
+
+	std::cout << "foo: " << foo.first << ", " << foo.second << '\n';	
+	std::cout << "bar: " << bar.first << ", " << bar.second << '\n';
+
+  return 0;
+	// ft::pair<std::string, std::string>("salut", "test");
+
+	// ft::map <std::string, std::string>::iterator yo;
+}
 
 // int	main(void)
 // {
@@ -85,105 +105,105 @@
 //   return 0;
 // }
 
-#define TESTED_TYPE int
-#define TESTED_NAMESPACE ft
-#define T_SIZE_TYPE typename TESTED_NAMESPACE::vector<T>::size_type
+// #define TESTED_TYPE int
+// #define TESTED_NAMESPACE ft
+// #define T_SIZE_TYPE typename TESTED_NAMESPACE::vector<T>::size_type
 
-template <typename T>
-void	printSize(TESTED_NAMESPACE::vector<T> const &vct, bool print_content = true)
-{
-	const T_SIZE_TYPE size = vct.size();
-	const T_SIZE_TYPE capacity = vct.capacity();
-	const std::string isCapacityOk = (capacity >= size) ? "OK" : "KO";
-	// Cannot limit capacity's max value because it's implementation dependent
+// template <typename T>
+// void	printSize(TESTED_NAMESPACE::vector<T> const &vct, bool print_content = true)
+// {
+// 	const T_SIZE_TYPE size = vct.size();
+// 	const T_SIZE_TYPE capacity = vct.capacity();
+// 	const std::string isCapacityOk = (capacity >= size) ? "OK" : "KO";
+// 	// Cannot limit capacity's max value because it's implementation dependent
 
-	std::cout << "size: " << size << std::endl;
-	std::cout << "capacity: " << isCapacityOk << std::endl;
-	std::cout << "max_size: " << vct.max_size() << std::endl;
-	if (print_content)
-	{
-		typename TESTED_NAMESPACE::vector<T>::const_iterator it = vct.begin();
-		typename TESTED_NAMESPACE::vector<T>::const_iterator ite = vct.end();
-		std::cout << std::endl << "Content is:" << std::endl;
-		for (; it != ite; ++it)
-			std::cout << "- " << *it << std::endl;
-	}
-	std::cout << "###############################################" << std::endl;
-}
+// 	std::cout << "size: " << size << std::endl;
+// 	std::cout << "capacity: " << isCapacityOk << std::endl;
+// 	std::cout << "max_size: " << vct.max_size() << std::endl;
+// 	if (print_content)
+// 	{
+// 		typename TESTED_NAMESPACE::vector<T>::const_iterator it = vct.begin();
+// 		typename TESTED_NAMESPACE::vector<T>::const_iterator ite = vct.end();
+// 		std::cout << std::endl << "Content is:" << std::endl;
+// 		for (; it != ite; ++it)
+// 			std::cout << "- " << *it << std::endl;
+// 	}
+// 	std::cout << "###############################################" << std::endl;
+// }
 
-void	prepost_incdec(TESTED_NAMESPACE::vector<TESTED_TYPE> &vct)
-{
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::iterator it = vct.begin();
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::iterator it_tmp;
+// void	prepost_incdec(TESTED_NAMESPACE::vector<TESTED_TYPE> &vct)
+// {
+// 	TESTED_NAMESPACE::vector<TESTED_TYPE>::iterator it = vct.begin();
+// 	TESTED_NAMESPACE::vector<TESTED_TYPE>::iterator it_tmp;
 
-	std::cout << "Pre inc" << std::endl;
-	it_tmp = ++it;
-	std::cout << *it_tmp << " | " << *it << std::endl;
+// 	std::cout << "Pre inc" << std::endl;
+// 	it_tmp = ++it;
+// 	std::cout << *it_tmp << " | " << *it << std::endl;
 
-	std::cout << "Pre dec" << std::endl;
-	it_tmp = --it;
-	std::cout << *it_tmp << " | " << *it << std::endl;
+// 	std::cout << "Pre dec" << std::endl;
+// 	it_tmp = --it;
+// 	std::cout << *it_tmp << " | " << *it << std::endl;
 
-	std::cout << "Post inc" << std::endl;
-	it_tmp = it++;
-	std::cout << *it_tmp << " | " << *it << std::endl;
+// 	std::cout << "Post inc" << std::endl;
+// 	it_tmp = it++;
+// 	std::cout << *it_tmp << " | " << *it << std::endl;
 
-	std::cout << "Post dec" << std::endl;
-	it_tmp = it--;
-	std::cout << *it_tmp << " | " << *it << std::endl;
-	std::cout << "###############################################" << std::endl;
-}
+// 	std::cout << "Post dec" << std::endl;
+// 	it_tmp = it--;
+// 	std::cout << *it_tmp << " | " << *it << std::endl;
+// 	std::cout << "###############################################" << std::endl;
+// }
 
-template <typename Ite_1, typename Ite_2>
-void ft_eq_ope(const Ite_1 &first, const Ite_2 &second, const bool redo = 1)
-{
-	std::cout << (first < second) << std::endl;
-	std::cout << (first <= second) << std::endl;
-	std::cout << (first > second) << std::endl;
-	std::cout << (first >= second) << std::endl;
-	if (redo)
-		ft_eq_ope(second, first, 0);
-}
+// template <typename Ite_1, typename Ite_2>
+// void ft_eq_ope(const Ite_1 &first, const Ite_2 &second, const bool redo = 1)
+// {
+// 	std::cout << (first < second) << std::endl;
+// 	std::cout << (first <= second) << std::endl;
+// 	std::cout << (first > second) << std::endl;
+// 	std::cout << (first >= second) << std::endl;
+// 	if (redo)
+// 		ft_eq_ope(second, first, 0);
+// }
 
-int		main(void)
-{
-const int size = 5;
-	TESTED_NAMESPACE::vector<TESTED_TYPE> vct(size);
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::reverse_iterator it_0(vct.rbegin());
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::reverse_iterator it_1(vct.rend());
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::reverse_iterator it_mid;
+// int		main(void)
+// {
+// const int size = 5;
+// 	TESTED_NAMESPACE::vector<TESTED_TYPE> vct(size);
+// 	TESTED_NAMESPACE::vector<TESTED_TYPE>::reverse_iterator it_0(vct.rbegin());
+// 	TESTED_NAMESPACE::vector<TESTED_TYPE>::reverse_iterator it_1(vct.rend());
+// 	TESTED_NAMESPACE::vector<TESTED_TYPE>::reverse_iterator it_mid;
 
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::const_reverse_iterator cit_0 = vct.rbegin();
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::const_reverse_iterator cit_1;
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::const_reverse_iterator cit_mid;
+// 	TESTED_NAMESPACE::vector<TESTED_TYPE>::const_reverse_iterator cit_0 = vct.rbegin();
+// 	TESTED_NAMESPACE::vector<TESTED_TYPE>::const_reverse_iterator cit_1;
+// 	TESTED_NAMESPACE::vector<TESTED_TYPE>::const_reverse_iterator cit_mid;
 
-	for (int i = size; it_0 != it_1; --i)
-		*it_0++ = i;
-	printSize(vct, 1);
-	it_0 = vct.rbegin();
-	cit_1 = vct.rend();
-	it_mid = it_0 + 3;
-	cit_mid = it_0 + 3; cit_mid = cit_0 + 3; cit_mid = it_mid;
+// 	for (int i = size; it_0 != it_1; --i)
+// 		*it_0++ = i;
+// 	printSize(vct, 1);
+// 	it_0 = vct.rbegin();
+// 	cit_1 = vct.rend();
+// 	it_mid = it_0 + 3;
+// 	cit_mid = it_0 + 3; cit_mid = cit_0 + 3; cit_mid = it_mid;
 
-	std::cout << std::boolalpha;
-	std::cout << ((it_0 + 3 == cit_0 + 3) && (cit_0 + 3 == it_mid)) << std::endl;
+// 	std::cout << std::boolalpha;
+// 	std::cout << ((it_0 + 3 == cit_0 + 3) && (cit_0 + 3 == it_mid)) << std::endl;
 
-	std::cout << "\t\tft_eq_ope:" << std::endl;
-	// regular it
-	ft_eq_ope(it_0 + 3, it_mid);
-	ft_eq_ope(it_0, it_1);
-	ft_eq_ope(it_1 - 3, it_mid);
-	// const it
-	ft_eq_ope(cit_0 + 3, cit_mid);
-	ft_eq_ope(cit_0, cit_1);
-	ft_eq_ope(cit_1 - 3, cit_mid);
-	// both it
-	ft_eq_ope(it_0 + 3, cit_mid);
-	ft_eq_ope(it_mid, cit_0 + 3);
-	ft_eq_ope(it_0, cit_1);
-	ft_eq_ope(it_1, cit_0);
-	ft_eq_ope(it_1 - 3, cit_mid);
-	ft_eq_ope(it_mid, cit_1 - 3);
+// 	std::cout << "\t\tft_eq_ope:" << std::endl;
+// 	// regular it
+// 	ft_eq_ope(it_0 + 3, it_mid);
+// 	ft_eq_ope(it_0, it_1);
+// 	ft_eq_ope(it_1 - 3, it_mid);
+// 	// const it
+// 	ft_eq_ope(cit_0 + 3, cit_mid);
+// 	ft_eq_ope(cit_0, cit_1);
+// 	ft_eq_ope(cit_1 - 3, cit_mid);
+// 	// both it
+// 	ft_eq_ope(it_0 + 3, cit_mid);
+// 	ft_eq_ope(it_mid, cit_0 + 3);
+// 	ft_eq_ope(it_0, cit_1);
+// 	ft_eq_ope(it_1, cit_0);
+// 	ft_eq_ope(it_1 - 3, cit_mid);
+// 	ft_eq_ope(it_mid, cit_1 - 3);
 
-	return (0);
-}
+// 	return (0);
+// }
--- utils/make_pair.hpp
+++ utils/make_pair.hpp
@@ -6,18 +6,21 @@
 /*   By: emaugale <emaugale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 01:37:28 by emaugale          #+#    #+#             */
-/*   Updated: 2022/05/25 01:38:29 by emaugale         ###   ########.fr       */
+/*   Updated: 2022/05/25 16:49:11 by emaugale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAKE_PAIR_HPP
 # define MAKE_PAIR_HPP
-# include "utils/pair.hpp"
+# include "pair.hpp"
 
 namespace ft
 {
     template< class T1, class T2 >
-    ft::pair<T1,T2> make_pair( T1 t, T2 u );
+    ft::pair<T1,T2> make_pair( T1 t, T2 u )
+    {
+        return (ft::pair<T1, T2>(t, u));
+    }
 }
 
 #endif
