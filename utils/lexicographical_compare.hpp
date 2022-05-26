c0e5a3d6febf3320246d827de064a1b16758fa2c
Elidjah MAUGALEM <emaugale@e1r11p13.42.fr>
2022-05-26 15:29:12 +0200

    Fixed my lexicographical comparaison

--- utils/lexicographical_compare.hpp
+++ utils/lexicographical_compare.hpp
@@ -6,7 +6,7 @@
 /*   By: emaugale <emaugale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 16:54:54 by emaugale          #+#    #+#             */
-/*   Updated: 2022/05/25 19:53:22 by emaugale         ###   ########.fr       */
+/*   Updated: 2022/05/26 15:17:11 by emaugale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,32 @@
 
 namespace ft
 {	
-template< class InputIt1, class InputIt2>
-bool lexicographical_compare( InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2 )
-{
-	for (; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2)
+	template< class InputIt1, class InputIt2>
+	bool lexicographical_compare( InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2 )
 	{
-		if (*first1 < *first1)
-			return (true);
-		if (*first2 < *first1)
-			return (false);
+		while (first1!=last1)
+		{
+			if (first2==last2 || *first2<*first1)
+				return (false);
+			else if (*first1<*first2)
+				return (true);
+			++first1; ++first2;
+		}
+		return (first2!=last2);
 	}
-	return ((first1 == last1) && (first2 != last2));
-}
 
-template <class InputIterator1, class InputIterator2, class Compare>
-bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, Compare comp)
-{
-	for ( ; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2 )
+	template <class InputIterator1, class InputIterator2, class Compare>
+	bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, Compare comp)
 	{
-		if (comp(*first1, *first2))
-			return (true);
-		if (comp(*first2, *first1))
-			return (false);
+		while (first1!=last1)
+		{
+			if (comp(*first1, *first2))
+				return (false);
+			else if (comp(*first2, *first1))
+				return (true);
+			++first1; ++first2;
+		}
+		return (first2!=last2);
 	}
-	return ((first1== last1) && (first2 != last2));
-}
 }
 #endif 
