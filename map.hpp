ad14afd5ee6c59b127e1c3821006a0cca3ee8552
Elidjah MAUGALEM <emaugale@e1r12p14.42.fr>
2022-05-27 18:32:22 +0200

    added binary_function and less

--- map.hpp
+++ map.hpp
@@ -6,7 +6,7 @@
 /*   By: emaugale <emaugale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 18:51:17 by emaugale          #+#    #+#             */
-/*   Updated: 2022/05/24 22:43:21 by emaugale         ###   ########.fr       */
+/*   Updated: 2022/05/27 18:16:19 by emaugale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@
 # include "iterators/reverse_iterator.hpp"
 # include "utils/type_traits.hpp"
 # include "utils/pair.hpp"
+# include "utils/less.hpp"
+# include "utils/red_black_tree.hpp"
 
 namespace ft
 {
@@ -29,19 +31,65 @@ namespace ft
 	class map
 	{
 		public:
-		typedef				T											value_type;
-		typedef				Allocator									allocator_type;
-		typedef typename	allocator_type::reference					reference;
-		typedef typename 	allocator_type::const_reference				const_reference;
-		typedef typename	allocator_type::pointer						pointer;
-		typedef typename	allocator_type::const_pointer				const_pointer;
+		typedef				Key														key_type;
+		typedef				T														mapped_type;
+		typedef				ft::pair<const key_type, mapped_type>					value_type;
+		typedef				Compare													key_compare;
+		typedef				Allocator												allocator_type;
+		typedef typename	allocator_type::reference								reference;
+		typedef typename 	allocator_type::const_reference							const_reference;
+		typedef typename	allocator_type::pointer									pointer;
+		typedef typename	allocator_type::const_pointer							const_pointer;
 		typedef	typename	ft::bidirectional_iterator<value_type>					iterator;
-		typedef	typename	ft::bidirectional_iterator<const value_type>		const_iterator;
-		typedef	typename	ft::reverse_iterator<iterator>				reverse_iterator;
-		typedef	typename	ft::reverse_iterator<const iterator>		const_reverse_iterator;
-		typedef	typename	allocator_type::difference_type				difference_type;
-		typedef	typename	allocator_type::size_type					size_type;
+		typedef	typename	ft::bidirectional_iterator<const value_type>			const_iterator;
+		typedef	typename	ft::reverse_iterator<iterator>							reverse_iterator;
+		typedef	typename	ft::reverse_iterator<const iterator>					const_reverse_iterator;
+		typedef	typename	allocator_type::difference_type							difference_type;
+		typedef	typename	allocator_type::size_type								size_type;
 		
+		/* ==================================================================================================== */
+		/*												Constructors											*/
+		/* ==================================================================================================== */
+
+		explicit map (const key_compare& comp = key_compare(), const allocator_type &alloc = allocator_type())
+		{
+			this->_allocator = alloc;
+			this->_size = 0;
+			this->_compare = comp;
+		}
+		template <class InputIterator>
+		map (InputIterator first, InputIterator last, const key_compare & comp = key_compare())
+		{
+			(void)first;
+			(void)last;
+			(void)comp;
+		}
+		map (const map & copy);
+		key_compare		key_comp(void) const {return this->_compare;};
+		/* ==================================================================================================== */
+		/*												Capacity											*/
+		/* ==================================================================================================== */
+		bool empty(void) const
+		{
+			if (this->_size == 0)
+				return (true);
+			return (false);	
+		}
+		
+		size_type	size(void) const
+		{
+			return (this->_size);
+		}
+		
+		size_type	max_size(void) const
+		{
+			return (this->_allocator.max_size());
+		}
+		private:
+		Compare									_compare;
+		allocator_type							_allocator;
+		size_type								_size;
+		RedBlackTree							_rbt;	
 	};
 }
 
---
+++ utils/binary_function.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   binary_function.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emaugale <emaugale@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/27 18:23:55 by emaugale          #+#    #+#             */
+/*   Updated: 2022/05/27 18:30:17 by emaugale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BINARY_FUNCTION_HPP
+# define BINARY_FUNCTION_HPP
+
+namespace ft
+{	
+	template<class Arg1, class Arg2, class Result>
+	struct binary_function
+	{
+		Arg1	first_argument_type;
+		Arg2	second_argument_type;
+		Result	result_type;
+	};
+}
+
+
+#endif
